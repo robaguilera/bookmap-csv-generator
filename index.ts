fa89f0f78114ee931bb8866c6c4a25e45c665e43
Robert Aguilera <robertaguilerajr@gmail.com>
2025-06-16 21:21:23 -0700

    chore: ran scripts for the day

--- index.ts
+++ index.ts
@@ -2,10 +2,10 @@ import { generateOhlcCSV } from "./src/csv";
 
 async function main() {
 	const esApiSymbol = "CME_MINI:ES1!";
-	const esCsvSymbols = ["ESM5.CME@RITHMIC", "MESM5.CME@RITHMIC"];
+	const esCsvSymbols = ["ESU5.CME@RITHMIC", "MESU5.CME@RITHMIC"];
 
 	const nqApiSymbol = "CME_MINI:NQ1!";
-	const nqCsvSymbols = ["NQM5.CME@RITHMIC", "MNQM5.CME@RITHMIC"];
+	const nqCsvSymbols = ["NQU5.CME@RITHMIC", "MNQU5.CME@RITHMIC"];
 
 	// Generate OHLC CSVs for ES symbols
 	await generateOhlcCSV(esApiSymbol, esCsvSymbols);
